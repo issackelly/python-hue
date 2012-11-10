5285becac9e81d9a40cb10ee3782e9a09cfd5ac9
Issac Kelly <issac.kelly@gmail.com>
2012-11-10 14:19:12 -0800

    First Commit

---
+++ README.rst
@@ -0,0 +1,28 @@
+==========
+Python-Hue
+==========
+
+Python client for the Philips Hue lighting system. I figured out almost all of
+this by monitoring the traffic on my router. I then found https://github.com/alistairg/IndigoHue/blob/master/Hue.indigoPlugin/Contents/Server%20Plugin/plugin.py
+which gave some clues about RGB support and the magic strings (select, lselect)
+to send alerts.
+
+Sample Usage is not docs::
+
+    from hue import Hue;
+    h = Hue(); # Initialize the class
+    h.get_state(); # Authenticate, bootstrap your lighting system
+    l = h.lights.get('l3') # get bulb #3
+    l.bri(0) # Dimmest
+    l.bri(255) # Brightest
+    l.rgb(120, 120, 0) # [0-255 rgb values]
+    l.rgb("#9af703") # Hex string
+    l.on()
+    l.off()
+    l.toggle()
+    l.alert() # short alert
+    l.alert("lselect") # long alert
+    l.setState({"bri": 220, "alert": "select"}) # Complex send
+
+Have fun! Let me know how you're using it.
+
---
+++ hue.py
@@ -0,0 +1,194 @@
+import requests
+import socket
+import json
+import datetime
+import logging
+from time import sleep
+import hashlib
+from colorpy import colormodels
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('hue')
+
+
+AUTH_FAILURE_RETRIES = 6
+AUTH_FAILURE_SLEEP = 10
+
+
+class Hue:
+    station_ip = "192.168.1.130"
+
+    # Hue appears to expect your username to be a 32 character hash
+    client_identifier = hashlib.md5("ph-%s" % socket.getfqdn()).hexdigest()
+
+    devicetype = "python-hue"
+    last_update_state = None
+    is_allowed = True
+    state = {}
+    lights = {}
+    groups = {}
+    schedules = {}
+    config = {}
+
+    def request(self, *args, **kwargs):
+        path = "http://%s/api/%s%s" % (
+            self.station_ip,
+            self.client_identifier,
+            kwargs.get("path", ""),
+        )
+        method = kwargs.get("method", "GET")
+        data = kwargs.get("data", None)
+
+        ## Needs more error checking, currently assumes connection will work
+        ## and that returns proper json.
+        resp = requests.request(method, path, data=data)
+
+        logger.debug(resp)
+        logger.debug(resp.content)
+
+        resp = json.loads(resp.content)
+
+        logger.debug(resp)
+
+        if isinstance(resp, list) and resp[0].get("error", None):
+            error = resp[0]["error"]
+            if error["type"] == 1:
+                ## Try to authenticate
+                if self.authenticate():
+                    return self.request(*args, **kwargs)
+                else:
+                    raise CouldNotAuthenticate()
+        else:
+            self.is_authenticated = True
+            return resp
+
+    def authenticate(self, tries=AUTH_FAILURE_RETRIES):
+        path = "http://%s/api" % (
+            self.station_ip
+        )
+
+        ## Needs more error checking, currently assumes connection will work
+        ## and that returns proper json.
+        auth = {
+            "devicetype": self.devicetype,
+            "username": self.client_identifier
+        }
+
+        resp = requests.post(path, data=json.dumps(auth))
+        logger.debug(resp)
+        logger.debug(resp.content)
+
+
+        resp = json.loads(resp.content)
+
+        logger.debug(resp)
+
+        logger.warn("Time to go press your button!")
+
+        if isinstance(resp, list) and resp[0].get("error", None):
+            logger.debug(resp[0]["error"])
+            if tries:
+                sleep(AUTH_FAILURE_SLEEP)
+                self.authenticate()
+            else:
+                raise TooManyFailures()
+
+        return True
+
+    def get_state(self):
+        self.last_resp = self.request()
+        state = self.last_resp
+        logger.debug(state)
+
+        self.state = state
+        self.config = state['config']
+        self.schedules = state['schedules']
+        self.groups = state['groups']
+
+        for l in state['lights']:
+            light = self.lights.get("l%s" % l, None)
+            if not light:
+                light = ExtendedColorLight(self, l)
+                self.lights["l%s" % l] = light
+            light.update_state_cache(state['lights'][l])
+
+        self.last_update_state = datetime.datetime.now()
+
+
+
+class ExtendedColorLight:
+    last_status_time = None
+    light_id = None
+    state = {}
+    hue = None
+
+    def __init__(self, hue, light_id):
+        self.hue = hue
+        self.light_id = light_id
+
+    def update_state_cache(self, values=None):
+        if not values:
+            values = self.hue.request(path = "/lights/%s" % self.light_id)
+
+        self.state.update(values)
+        self.last_status_time = datetime.datetime.now()
+
+    def set_state(self, state):
+        self.hue.request(path="/lights/%s/state" % self.light_id, method="PUT", data=json.dumps(state))
+        self.update_state_cache()
+        return self
+
+    def on(self):
+        return self.set_state({"on": True})
+
+    def off(self):
+        return self.set_state({"off": True})
+
+    def bri(self, level):
+        # level between 0 and 255
+        return self.set_state({"bri": level})
+
+    def toggle(self):
+        self.update_state_cache()
+        if self.state["on"]:
+            self.off()
+        else:
+            self.on()
+
+    def alert(self, type="select"):
+        return self.set_state({"alert": type})
+
+    def xy(self, x, y):
+        return self.set_state({"xy": [x, y]})
+
+    def rgb(self, red, green=None, blue=None):
+        if isinstance(red, basestring):
+            # assume a hex string is passed
+            rstring = red
+            red = int(rstring[1:3], 16)
+            green = int(rstring[3:5], 16)
+            blue = int(rstring[5:], 16)
+
+        print red, green, blue
+
+        # We need to convert the RGB value to Yxy.
+        redScale = float(red) / 255.0
+        greenScale = float(green) / 255.0
+        blueScale = float(blue) / 255.0
+        colormodels.init(phosphor_red=colormodels.xyz_color(0.64843, 0.33086), phosphor_green=colormodels.xyz_color(0.4091,0.518), phosphor_blue=colormodels.xyz_color(0.167, 0.04))
+        logger.debug(redScale, greenScale, blueScale)
+        xyz = colormodels.irgb_color(red, green, blue)
+        logger.debug(xyz)
+        xyz = colormodels.xyz_from_rgb(xyz)
+        logger.debug(xyz)
+        xyz = colormodels.xyz_normalize(xyz)
+        logger.debug(xyz)
+
+        return self.set_state({"xy": [xyz[0], xyz[1]]})
+
+
+class TooManyFailures(Exception):
+    pass
+
+class CouldNotAuthenticate(Exception):
+    pass
